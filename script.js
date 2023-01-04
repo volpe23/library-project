50333beca5a0bf12820da128e3399f57f37c6b2c
volpe23 <arozmianecs@gmail.com>
2023-01-04 16:03:37 +0200

    Basic setup and styling done

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-const content = document.querySelector('#content');
-// console.log(content)
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
     this.info = () => `${title} by ${author}, ${pages} pages, ${read ? 'has been read' : 'not read yet'}`;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, true);
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
 
-console.log(theHobbit.title);
-content.textContent = theHobbit.info();
+const harryPotter = new Book("Harry Potter", "JKR", 300, false);
+
+const harryPotter2 = new Book("Harry Potter", "JKR", 300, false);
+
+const harryPotter3 = new Book("Harry Potter", "JKR", 300, false);
+myLibrary.push(harryPotter);
+myLibrary.push(harryPotter2);
+myLibrary.push(harryPotter3);
+
+function displayBooks() {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    myLibrary.forEach((book) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const title = document.createElement('h3');
+        title.textContent = book.title;
+        card.append(title);
+        container.append(card);
+    });
+}
+const newBookButton = document.querySelector('#newBook');
+newBookButton.addEventListener('click', (event) => {
+    const bookTitle = document.querySelector('#title').value;
+    const bookAuthor = document.querySelector('#author').value;
+    const bookPages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#read').value;
+
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, isRead);
+    event.preventDefault();
+    displayBooks();
+});
+
+displayBooks();
